df15578d2d708444acb54ebbdb822254e5edfeaf
Tugas 2 <rizmarahmadani84@gmail.com>
2024-09-15 16:21:50 +0800

    Create Penjumlahan.py

---
+++ Penjumlahan.py
@@ -0,0 +1,4 @@
+bilangan1 = float(input("Masukkan bilangan pertama: "))
+bilangan2 = float(input("Masukkan bilangan kedua: "))
+hasil = bilangan1 + bilangan2
+print(f"Hasil penjumlahan {bilangan1} + {bilangan2} = {hasil}")
