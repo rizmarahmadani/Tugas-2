f3c27df86a2ec445780a45e8acafea5508a8e894
Tugas 2 <rizmarahmadani84@gmail.com>
2024-09-15 16:20:20 +0800

    Create Luas lingkaran.py

---
+++ Luas lingkaran.py
@@ -0,0 +1,5 @@
+jari_jari = float(input("Masukkan jari-jari lingkaran: "))
+phi = 3.14
+luas = phi * (jari_jari ** 2)
+print(f"Luas lingkaran dengan jari-jari {jari_jari} adalah {luas}"
+      angka 0
